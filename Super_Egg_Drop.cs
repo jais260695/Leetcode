cf4838860f32b400c4155b6ea71f9f08320527fe
jais260695 <aj936563@gmail.com>
2021-05-15 10:59:45 +0530

    Create Super_Egg_Drop.cs
    You are given k identical eggs and you have access to a building with n floors labeled from 1 to n.
    
    You know that there exists a floor f where 0 <= f <= n such that any egg dropped at a floor higher than f will break, and any egg dropped at or below floor f will not break.
    
    Each move, you may take an unbroken egg and drop it from any floor x (where 1 <= x <= n). If the egg breaks, you can no longer use it. However, if the egg does not break, you may reuse it in future moves.
    
    Return the minimum number of moves that you need to determine with certainty what the value of f is.
    
    Example 1:
    
    Input: k = 1, n = 2
    Output: 2
    Explanation:
    Drop the egg from floor 1. If it breaks, we know that f = 0.
    Otherwise, drop the egg from floor 2. If it breaks, we know that f = 1.
    If it does not break, then we know f = 2.
    Hence, we need at minimum 2 moves to determine with certainty what the value of f is.
    Example 2:
    
    Input: k = 2, n = 6
    Output: 3
    Example 3:
    
    Input: k = 3, n = 14
    Output: 4
    
    Constraints:
    
    1 <= k <= 100
    1 <= n <= 104

---
+++ Super_Egg_Drop.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int SuperEggDrop(int K, int N) {
+        int[] dp1 = new int[N+1];//for K-1
+        int[] dp2 = new int[N+1];//for K
+        for(int i=1;i<=N;i++)
+        {
+            dp1[i] = i;//for K=1
+        }
+         
+        for(int k=2;k<=K;k++)
+        {
+            int x = 1;
+            for(int n=1;n<=N;n++)
+            {
+                //Binary search can also be used
+                while(x<n)
+                {
+                    if(Math.Max(dp1[x-1],dp2[n-x])<Math.Max(dp1[x],dp2[n-x-1])) break;
+                    x++;
+                }
+                
+               dp2[n] = 1 + Math.Max(dp1[x-1],dp2[n-x]);
+            }
+            dp1 = dp2;
+            dp2 = new int[N+1];
+        }
+        return dp1[N];
+            
+    }
+}
