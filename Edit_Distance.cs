ae96dea7eb27987e41c67aeeeef7d7111f050de7
jais260695 <aj936563@gmail.com>
2021-05-23 19:06:34 +0530

    Create Edit_Distance.cs
    Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
    
    You have the following three operations permitted on a word:
    
    Insert a character
    Delete a character
    Replace a character
    
    Example 1:
    
    Input: word1 = "horse", word2 = "ros"
    Output: 3
    Explanation:
    horse -> rorse (replace 'h' with 'r')
    rorse -> rose (remove 'r')
    rose -> ros (remove 'e')
    Example 2:
    
    Input: word1 = "intention", word2 = "execution"
    Output: 5
    Explanation:
    intention -> inention (remove 't')
    inention -> enention (replace 'i' with 'e')
    enention -> exention (replace 'n' with 'x')
    exention -> exection (replace 'n' with 'c')
    exection -> execution (insert 'u')
    
    Constraints:
    
    0 <= word1.length, word2.length <= 500
    word1 and word2 consist of lowercase English letters.

---
+++ Edit_Distance.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int MinDistance(string word1, string word2) {
+        int n1 = word2.Count();
+        int n2 = word1.Count();
+        
+        int[,] dp = new int[n1+1,n2+1];
+        
+        dp[0,0] = 0;
+        for(int i=1;i<=n1;i++)
+        {
+            dp[i,0] = i;
+        }
+        for(int i=1;i<=n2;i++)
+        {
+            dp[0,i] = i;
+        }
+        
+        for(int i=1;i<=n2;i++)
+        {
+            for(int j=1;j<=n1;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                {
+                    dp[j,i] = dp[j-1,i-1];
+                }
+                else
+                {
+                    dp[j,i] = 1 + Math.Min(Math.Min(dp[j,i-1],dp[j-1,i]),dp[j-1,i-1]);
+                }
+            }
+        }
+        return dp[n1,n2];
+    }
+}
