87cb2b0656cc38cccdba234b072a3902feeac255
jais260695 <aj936563@gmail.com>
2021-06-18 21:41:39 +0530

    Create Count_Primes.cs
    Count the number of prime numbers less than a non-negative number, n.
    
    Example 1:
    
    Input: n = 10
    Output: 4
    Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
    Example 2:
    
    Input: n = 0
    Output: 0
    Example 3:
    
    Input: n = 1
    Output: 0
    
    Constraints:
    
    0 <= n <= 5 * 106

---
+++ Count_Primes.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int CountPrimes(int n) {
+        if(n<=2) return 0;
+        bool[] isPrime = new bool[n];
+        
+        for(int i=2;i<n;i++)
+        {
+            isPrime[i] = true;
+        }
+        
+        for(int i=2;i*i<n;i++)
+        {
+            if(isPrime[i])
+            {
+                for(int j = i*i;j<n;j+=i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        int count = 0;
+        for(int i=2;i<n;i++)
+        {
+            if(isPrime[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
