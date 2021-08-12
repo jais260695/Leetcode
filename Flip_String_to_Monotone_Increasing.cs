2309cd1527348cbdb74455f16db3b23156379e93
jais260695 <aj936563@gmail.com>
2021-08-12 09:44:55 +0530

    Create Flip_String_to_Monotone_Increasing.cs
    A binary string is monotone increasing if it consists of some number of 0's (possibly none), followed by some number of 1's (also possibly none).
    
    You are given a binary string s. You can flip s[i] changing it from 0 to 1 or from 1 to 0.
    
    Return the minimum number of flips to make s monotone increasing.
    
    Example 1:
    
    Input: s = "00110"
    Output: 1
    Explanation: We flip the last digit to get 00111.
    Example 2:
    
    Input: s = "010110"
    Output: 2
    Explanation: We flip to get 011111, or alternatively 000111.
    Example 3:
    
    Input: s = "00011000"
    Output: 2
    Explanation: We flip to get 00000000.
    
    Constraints:
    
    1 <= s.length <= 105
    s[i] is either '0' or '1'.

---
+++ Flip_String_to_Monotone_Increasing.cs
@@ -0,0 +1,46 @@
+public class Solution {    
+    public int[] prefixOnesCount;
+    
+    public int[] dp;
+    
+    public int Solve(int r,string s)
+    {
+        if(r==0)
+        {
+            return 0;
+        }
+        if(dp[r]!=-1) return dp[r];
+        if(s[r]=='1')
+        {
+            return Solve(r-1,s);
+        }
+        else
+        {
+            return Math.Min(1+Solve(r-1,s),prefixOnesCount[r-1]);
+        }
+    }
+    
+    public int MinFlipsMonoIncr(string s) {
+        int n = s.Length;
+        if(s.Length==1) return 0;
+        
+        dp = new int[n];
+        prefixOnesCount = new int[n];
+        
+        if(s[0]=='1')
+        {
+            prefixOnesCount[0] = 1;
+        }
+        dp[0] = -1;
+        for(int i = 1;i<n;i++)
+        {
+            prefixOnesCount[i] = prefixOnesCount[i-1];
+            if(s[i]=='1')
+            {
+                prefixOnesCount[i]+=1;
+            }
+            dp[i] = -1;
+        }
+        return Solve(n-1,s);
+    }
+}
