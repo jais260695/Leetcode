321d6456ceefb133e777b3440dbdf7a9bc776ccc
jais260695 <aj936563@gmail.com>
2021-09-08 10:17:06 +0530

    Create Minimum_Adjacent_Swaps_to_Reach_the_Kth_Smallest_Number.cs
    You are given a string num, representing a large integer, and an integer k.
    
    We call some integer wonderful if it is a permutation of the digits in num and is greater in value than num. There can be many wonderful integers. However, we only care about the smallest-valued ones.
    
    For example, when num = "5489355142":
    The 1st smallest wonderful integer is "5489355214".
    The 2nd smallest wonderful integer is "5489355241".
    The 3rd smallest wonderful integer is "5489355412".
    The 4th smallest wonderful integer is "5489355421".
    Return the minimum number of adjacent digit swaps that needs to be applied to num to reach the kth smallest wonderful integer.
    
    The tests are generated in such a way that kth smallest wonderful integer exists.
    
    Example 1:
    
    Input: num = "5489355142", k = 4
    Output: 2
    Explanation: The 4th smallest wonderful number is "5489355421". To get this number:
    - Swap index 7 with index 8: "5489355142" -> "5489355412"
    - Swap index 8 with index 9: "5489355412" -> "5489355421"
    Example 2:
    
    Input: num = "11112", k = 4
    Output: 4
    Explanation: The 4th smallest wonderful number is "21111". To get this number:
    - Swap index 3 with index 4: "11112" -> "11121"
    - Swap index 2 with index 3: "11121" -> "11211"
    - Swap index 1 with index 2: "11211" -> "12111"
    - Swap index 0 with index 1: "12111" -> "21111"
    Example 3:
    
    Input: num = "00123", k = 1
    Output: 1
    Explanation: The 1st smallest wonderful number is "00132". To get this number:
    - Swap index 3 with index 4: "00123" -> "00132"
    
    Constraints:
    
    2 <= num.length <= 1000
    1 <= k <= 1000
    num only consists of digits.

---
+++ Minimum_Adjacent_Swaps_to_Reach_the_Kth_Smallest_Number.cs
@@ -0,0 +1,93 @@
+public class Solution {
+    public int JustGreater(int l, int r, int key, int[] nums)
+    {
+        while(l<=r)
+        {
+            int mid = l + (r-l)/2;
+            if(nums[mid]<=key)
+            {
+                r = mid-1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }  
+        return r;
+    }
+    
+    public void Swap(int i, int j, int[] nums)
+    {
+        int temp = nums[i];
+        nums[i]= nums[j];
+        nums[j] = temp;
+        return;
+    }
+    
+    public void Reverse(int l, int r, int[] nums)
+    {
+        int i = l;
+        int j = r;
+        while(i<j)
+        {
+            Swap(i,j,nums);
+            i++;
+            j--;
+        }
+    }
+    
+    public void NextPermutation(int[] nums) {
+        int n = nums.Count();
+        int i = n-2;        
+        while(i>=0 && nums[i+1]<=nums[i])
+        {
+            i--;
+        }        
+        if(i<0)
+        {
+            Reverse(0,n-1,nums);
+            return;
+        }       
+        int justGreater = JustGreater(i+1,n-1,nums[i],nums);
+        Swap(i,justGreater,nums);
+        Reverse(i+1,n-1,nums);
+    }
+    public int GetMinSwaps(string num, int k) {
+        int n = num.Length;
+        int[] nums = new int[n];
+        int[] temp = new int[n];
+        int i =0;
+        for(i=0;i<n;i++)
+        {
+            nums[i] = num[i] - '0';
+            temp[i] = num[i] - '0';
+        }
+        
+        for(i = 0;i<k;i++)
+        {
+            NextPermutation(nums);
+        }
+        i = 0;
+        while(i<n && nums[i]==temp[i])
+        {
+            i++;
+        }
+        int result = 0;
+        for(int j=i;j<n-1;j++)
+        {
+            int l = j;
+            while(l<n && nums[l]!=temp[j])
+            {
+                l++;
+            }
+            if(l==n) break;
+            while(l>j)
+            {
+                Swap(l,l-1,nums);
+                l--;
+                result++;
+            }
+        }
+        return result;
+    }
+}
