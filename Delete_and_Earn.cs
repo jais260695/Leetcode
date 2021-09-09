e92a3a0f32b6712daedf0b971e7c9b9808e8d61d
jais260695 <aj936563@gmail.com>
2021-09-09 09:20:02 +0530

    Create Delete_and_Earn.cs
    You are given an integer array nums. You want to maximize the number of points you get by performing the following operation any number of times:
    
    Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you must delete every element equal to nums[i] - 1 and every element equal to nums[i] + 1.
    Return the maximum number of points you can earn by applying the above operation some number of times.
    
    Example 1:
    
    Input: nums = [3,4,2]
    Output: 6
    Explanation: You can perform the following operations:
    - Delete 4 to earn 4 points. Consequently, 3 is also deleted. nums = [2].
    - Delete 2 to earn 2 points. nums = [].
    You earn a total of 6 points.
    Example 2:
    
    Input: nums = [2,2,3,3,3,4]
    Output: 9
    Explanation: You can perform the following operations:
    - Delete a 3 to earn 3 points. All 2's and 4's are also deleted. nums = [3,3].
    - Delete a 3 again to earn 3 points. nums = [3].
    - Delete a 3 once more to earn 3 points. nums = [].
    You earn a total of 9 points.
    
    Constraints:
    
    1 <= nums.length <= 2 * 104
    1 <= nums[i] <= 104

---
+++ Delete_and_Earn.cs
@@ -0,0 +1,43 @@
+public class Solution {
+    public int DeleteAndEarn(int[] nums) {
+        Dictionary<int,int> map = new Dictionary<int,int>();
+        int n = nums.Count();
+        if(n==1) return nums[0];
+        List<int> list = new List<int>();
+        for(int i=0;i<n;i++)
+        {
+            if(!map.ContainsKey(nums[i]))
+            {
+                map.Add(nums[i],0);
+                list.Add(nums[i]);
+            }
+            map[nums[i]]++;
+        }
+        n = list.Count();
+        list.Sort();
+        int[] dp = new int[n];
+        dp[0] = list[0]*map[list[0]];
+        dp[1] = list[1]*map[list[1]];
+        if(list[0]+1!=list[1])
+        {
+            dp[1]+=dp[0];
+        }
+        else
+        {
+            dp[1] = Math.Max(dp[1],dp[0]);
+        }
+        for(int i=2;i<n;i++)
+        {
+            dp[i] = list[i]*map[list[i]];
+            if(list[i-1]+1 != list[i])
+            {
+                dp[i]+=dp[i-1];
+            }
+            else
+            {
+                dp[i] = Math.Max(dp[i]+dp[i-2],dp[i-1]);   
+            }
+        }
+        return dp[n-1];
+    }
+}
