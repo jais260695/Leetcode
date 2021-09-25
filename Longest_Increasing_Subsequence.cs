a2cdbfb4f5c087042d605bb5195690a62d014fe7
jais260695 <aj936563@gmail.com>
2021-09-25 08:19:23 +0530

    Create Longest_Increasing_Subsequence.cs
    Given an integer array nums, return the length of the longest strictly increasing subsequence.
    
    A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
    
    Example 1:
    
    Input: nums = [10,9,2,5,3,7,101,18]
    Output: 4
    Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
    Example 2:
    
    Input: nums = [0,1,0,3,2,3]
    Output: 4
    Example 3:
    
    Input: nums = [7,7,7,7,7,7,7]
    Output: 1
    
    Constraints:
    
    1 <= nums.length <= 2500
    -104 <= nums[i] <= 104
    
    Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?

---
+++ Longest_Increasing_Subsequence.cs
@@ -0,0 +1,22 @@
+    public class Solution {
+    public int LengthOfLIS(int[] nums) {
+        int n = nums.Count();
+        if(n==0) return 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for(int i=1;i<n;i++)
+        {
+            dp[i]=1;
+            for(int j=i-1;j>=0;j--)
+            {
+                
+                if(nums[i]>nums[j] && dp[i]<=dp[j])
+                {
+                    dp[i] = dp[j]+1;
+                }
+            }
+            Console.Write(dp[i]+" ");
+        }
+        return dp.Max();
+    }
+}
