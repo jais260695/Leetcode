4866ca2f94553bfa78d6c9c5d32537bd204013da
jais260695 <aj936563@gmail.com>
2021-10-02 07:57:01 +0530

    Create Find_All_Anagrams_in_a_String.cs
    Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
    
    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
    
    Example 1:
    
    Input: s = "cbaebabacd", p = "abc"
    Output: [0,6]
    Explanation:
    The substring with start index = 0 is "cba", which is an anagram of "abc".
    The substring with start index = 6 is "bac", which is an anagram of "abc".
    Example 2:
    
    Input: s = "abab", p = "ab"
    Output: [0,1,2]
    Explanation:
    The substring with start index = 0 is "ab", which is an anagram of "ab".
    The substring with start index = 1 is "ba", which is an anagram of "ab".
    The substring with start index = 2 is "ab", which is an anagram of "ab".
    
    Constraints:
    
    1 <= s.length, p.length <= 3 * 104
    s and p consist of lowercase English letters.

---
+++ Find_All_Anagrams_in_a_String.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public bool CompareArrays(int[] arr1, int[] arr2)
+    {
+        for(int i=0;i<26;i++)
+        {
+            if(arr1[i]!=arr2[i]) return false;
+        }
+        return true;
+    }
+    public IList<int> FindAnagrams(string s, string p) {
+        int[] countOfCharsInP = new int[26];
+        foreach(char ch in p)
+        {
+            countOfCharsInP[ch-'a']++;
+        }
+        int n = p.Length;
+        int m = s.Length;
+        int i = 0;
+        int j = n-1;
+        List<int> result = new List<int>();
+        while(j<m)
+        {
+            int[] tempCount = new int[26];
+            for(int k = i;k<=j;k++)
+            {   
+                tempCount[s[k]-'a']++;   
+            }
+            if(CompareArrays(countOfCharsInP,tempCount))
+            {
+                result.Add(i);
+            }
+            i++;
+            j++;
+        }
+        return result.ToList<int>();
+    }
+}
