a66ad6b87d5d852f4c35b33975bf3f0723078c6b
jais260695 <aj936563@gmail.com>
2021-10-06 06:41:43 +0530

    Create Implement_Queue_using_Stacks.cs
    Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
    
    Implement the MyQueue class:
    
    void push(int x) Pushes element x to the back of the queue.
    int pop() Removes the element from the front of the queue and returns it.
    int peek() Returns the element at the front of the queue.
    boolean empty() Returns true if the queue is empty, false otherwise.
    Notes:
    
    You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
    Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
    
    Example 1:
    
    Input
    ["MyQueue", "push", "push", "peek", "pop", "empty"]
    [[], [1], [2], [], [], []]
    Output
    [null, null, null, 1, 1, false]
    
    Explanation
    MyQueue myQueue = new MyQueue();
    myQueue.push(1); // queue is: [1]
    myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
    myQueue.peek(); // return 1
    myQueue.pop(); // return 1, queue is [2]
    myQueue.empty(); // return false
    
    Constraints:
    
    1 <= x <= 9
    At most 100 calls will be made to push, pop, peek, and empty.
    All the calls to pop and peek are valid.
    
    Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer.

---
+++ Implement_Queue_using_Stacks.cs
@@ -0,0 +1,61 @@
+public class MyQueue {
+    Stack<int> q1 = new Stack<int>();
+    Stack<int> q2 = new Stack<int>();
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    public void Push(int x) {
+            q1.Push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int Pop() {
+            while(q1.Count()!=0)
+            {
+                q2.Push(q1.Pop());
+            }
+         int val = q2.Pop();
+        
+        while(q2.Count()!=0)
+        {
+            q1.Push(q2.Pop());
+        }
+        
+        return val;
+    }
+    
+    /** Get the front element. */
+    public int Peek() {
+            while(q1.Count()!=0)
+            {
+                q2.Push(q1.Pop());
+            }
+            int val = q2.Peek();
+        
+        while(q2.Count()!=0)
+        {
+            q1.Push(q2.Pop());
+        }
+        return val;
+    }
+    
+    /** Returns whether the queue is empty. */
+    public bool Empty() {
+        if(q1.Count()==0 && q2.Count()==0)
+            return true;
+        return false;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Peek();
+ * bool param_4 = obj.Empty();
+ */
