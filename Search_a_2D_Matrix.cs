3a8642c28f218834e9e7d17c816db773d9ae1072
jais260695 <aj936563@gmail.com>
2021-10-15 10:26:21 +0530

    Create Search_a_2D_Matrix.cs
    Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
    
    Integers in each row are sorted from left to right.
    The first integer of each row is greater than the last integer of the previous row.
    
    Example 1:
    
    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
    Output: true
    Example 2:
    
    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
    Output: false
    
    Constraints:
    
    m == matrix.length
    n == matrix[i].length
    1 <= m, n <= 100
    -104 <= matrix[i][j], target <= 104

---
+++ Search_a_2D_Matrix.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public bool Search(int[] arr, int key, int m)
+    {
+        int l = 0;
+        int r = m-1;
+        while(l<=r)
+        {
+            int mid = l + (r-l)/2;
+            if(arr[mid]==key)
+            {
+                return true;
+            }
+            else if(arr[mid]>key)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid+1;
+            }
+        }
+        return false;
+    }
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int n = matrix.Count();
+        int m = matrix[0].Count();
+        for(int i=0;i<n;i++)
+        {
+            if(target>=matrix[i][0] && target<=matrix[i][m-1])
+            {
+                return Search(matrix[i],target,m);
+            }
+        }
+        return false;
+    }
+}
