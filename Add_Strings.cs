043e314649e1611cb906b98a99c23c1bc7b5f629
jais260695 <aj936563@gmail.com>
2021-10-17 08:57:50 +0530

    Create Add_Strings.cs
    Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
    
    You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
    
    Example 1:
    
    Input: num1 = "11", num2 = "123"
    Output: "134"
    Example 2:
    
    Input: num1 = "456", num2 = "77"
    Output: "533"
    Example 3:
    
    Input: num1 = "0", num2 = "0"
    Output: "0"
    
    Constraints:
    
    1 <= num1.length, num2.length <= 104
    num1 and num2 consist of only digits.
    num1 and num2 don't have any leading zeros except for the zero itself.

---
+++ Add_Strings.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public string AddStrings(string num1, string num2) {
+        string ans ="";
+        int c=0;
+        int n1 = num1.Length;
+        int n2 = num2.Length;
+        int i=n1-1;
+        int j=n2-1;
+        while(i>=0 && j>=0)
+        {
+            int x = num1[i]-'0';
+            int y = num2[j]-'0';
+            
+            int z = x + y + c;
+            ans=(z%10) + ans;
+            c = z/10;
+            i--;
+            j--;
+        }
+        while(i>=0)
+        {
+            int x = num1[i]-'0';
+            int z = x + c;
+            ans=(z%10) + ans;
+            c = z/10;
+            i--;
+        }
+        while(j>=0)
+        {
+            int y = num2[j]-'0';
+            int z = y + c;
+            ans=(z%10) + ans;
+            c = z/10;
+            j--;
+        }
+        return c>0?c+ans:ans;
+    }
+}
