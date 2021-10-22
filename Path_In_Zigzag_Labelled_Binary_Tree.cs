7d250659209b5357b7bb7b2f5424e3b883b7cc3f
jais260695 <aj936563@gmail.com>
2021-10-22 08:39:12 +0530

    Update Path_In_Zigzag_Labelled_Binary_Tree.cs

--- Path_In_Zigzag_Labelled_Binary_Tree.cs
+++ Path_In_Zigzag_Labelled_Binary_Tree.cs
@@ -3,31 +3,18 @@ public class Solution {
         List<int> ans = new List<int>();
         ans.Add(label);
         int temp = label;
-        int count = 0;
+        int count = 1;
         while(temp>1)
         {
             temp>>=1;
-            count++;
+            count<<=1;
         }
-        temp=1<<count;
+        count=count-1;
         while(label>1)
-        {
-            
-            int parent = (label/2);
-            int realParent = 0;
-            count=0;
-            while(parent>1)
-            {
-                if((parent&1)==0)
-                {
-                    realParent+= 1<<count;
-                }
-                parent>>=1;
-                count++;
-            }
-            realParent+=(1<<count);
-            ans.Add(realParent);
-            label = realParent;
+        {        
+            count>>=1;
+            label = (count)^(label/2);
+            ans.Add(label);
         }
         ans.Reverse();
         return ans.ToList<int>();
