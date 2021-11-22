480cd81d0aa36c6f97d60b145d8edc56492d678b
jais260695 <aj936563@gmail.com>
2021-11-22 08:44:16 +0530

    Update Daily_Temperatures.cs

--- Daily_Temperatures.cs
+++ Daily_Temperatures.cs
@@ -3,26 +3,15 @@ public class Solution {
         int n = temperatures.Count();
         int[] res = new int[n];
         Stack<int> st = new Stack<int>();
-        st.Push(n-1);
-        res[n-1] = 0;
-        for(int i=n-2;i>=0;i--)
+        for(int i=0;i<n;i++)
         {
-            while(st.Count()>0 && temperatures[st.Peek()]<=temperatures[i])
+            while(st.Any() && temperatures[i]>temperatures[st.Peek()])
             {
-                st.Pop();
+                int j = st.Pop();
+                res[j] = i-j;
             }
-            if(st.Count()==0)
-            {
-                res[i] = 0;
-            }
-            else
-            {
-                res[i] = st.Peek()-i;
-            }
-            
             st.Push(i);
         }
-        
         return res;
     }
 }
