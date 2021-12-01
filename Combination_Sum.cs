ee5394e99690f768c4dd7ced27882f3236f9b6b7
jais260695 <aj936563@gmail.com>
2021-12-01 09:19:50 +0530

    Update Combination_Sum.cs

--- Combination_Sum.cs
+++ Combination_Sum.cs
@@ -1,29 +1,26 @@
 public class Solution {
     List<List<int>> result = new List<List<int>>();
     List<int> res = new List<int>();
-    public void Solve(int[] candidates, int target, int n,int sum, int j)
+    public void Solve(int[] candidates, int target, int n, int curr)
     {
-        if(sum==target)
+        if(target<0) return;
+        
+        if(target==0)
         {
             result.Add(new List<int>(res));
             return;
         }
         
-        for(int i =j ;i< n;i++)
+        for(int i = curr ;i< n;i++)
         {
-            if(sum+candidates[i]<=target)
-            {
-                sum+=candidates[i];
-                res.Add(candidates[i]);
-                Solve(candidates,target,n,sum,i);
-                res.RemoveAt(res.Count()-1);
-                sum-=candidates[i];
-            }
+            res.Add(candidates[i]);
+            Solve(candidates,target-candidates[i],n,i);
+            res.RemoveAt(res.Count()-1);
         }
     }
     
     public IList<IList<int>> CombinationSum(int[] candidates, int target) {
-        Solve(candidates,target,candidates.Count(), 0,0);
+        Solve(candidates,target,candidates.Count(),0);
         return result.ToList<IList<int>>();
     }
 }
