49c139a003aa089eba9217861a6df1fd737dee43
jais260695 <aj936563@gmail.com>
2021-12-06 19:42:52 +0530

    Update Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cs

--- Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cs
+++ Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cs
@@ -1,14 +1,29 @@
 public class Solution {
+    int[,] dp;
+    public int Solve(int i, int[] prices, int fee,int flag)
+    {
+        if(i>=prices.Count()) return 0;
+        
+        if(dp[i,flag]!=-1) return dp[i,flag];
+        
+        if(flag==1)
+        {
+            dp[i,flag] = Math.Max(Solve(i+1,prices,fee,1),Solve(i+1,prices,fee,0)+prices[i]-fee);
+        }
+        else
+        {
+            dp[i,flag] = Math.Max(Solve(i+1,prices,fee,1)-prices[i],Solve(i+1,prices,fee,0));
+        }
+        return dp[i,flag];
+    }
     public int MaxProfit(int[] prices, int fee) {
         int n =  prices.Count();
-        int[] dp = new int[n+1];
-        int dp1 = -prices[0];
-        for(int i=1;i<n;i++)
+        dp = new int[n,2];
+        for(int i=0;i<n;i++)
         {
-            
-            dp1 = Math.Max(dp1,dp[i-1]-prices[i-1]);
-            dp[i+1] = Math.Max(dp[i],dp1 - fee + prices[i] );
+            dp[i,0] = -1;
+            dp[i,1] = -1;
         }
-        return dp[n];
+        return Math.Max(Solve(1,prices,fee,1)-prices[0],Solve(1,prices,fee,0));
     }
 }
