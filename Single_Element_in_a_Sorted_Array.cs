e50d7a4132e5e1710fcc0419f411af743dce84a6
jais260695 <aj936563@gmail.com>
2021-12-08 18:24:53 +0530

    Update Single_Element_in_a_Sorted_Array.cs

--- Single_Element_in_a_Sorted_Array.cs
+++ Single_Element_in_a_Sorted_Array.cs
@@ -1,32 +1,31 @@
 public class Solution {
     public int SingleNonDuplicate(int[] nums) {
-        int l=0;
         int n = nums.Count();
-        if(n==1) return nums[0];
-        int h = n-1;
+        int l=0,h = n-1;
         while(l<=h)
         {
             int mid = l+(h-l)/2;
-            if( (mid-1<0|| nums[mid-1]!=nums[mid]) && (mid+1==n || nums[mid+1]!=nums[mid]))
+            if((mid-1<0 || nums[mid-1] != nums[mid]) && 
+               (mid+1==n || nums[mid+1] != nums[mid]))
                 return nums[mid];
             else if(mid%2==1)
             {
-                if(mid-1>=0 && nums[mid-1]==nums[mid])
+                if(nums[mid-1]==nums[mid])
                 {
                     l = mid + 1;
                 }
-                if(mid+1<n && nums[mid+1]==nums[mid])
+                else
                 {
                     h = mid-1;
                 }
             }
             else
             {
-                if(mid-1>=0 && nums[mid-1]==nums[mid])
+                if(nums[mid-1]==nums[mid])
                 {
                     h = mid - 1;
                 }
-                if(mid+1<n && nums[mid+1]==nums[mid])
+                else
                 {
                     l = mid+1;
                 }
