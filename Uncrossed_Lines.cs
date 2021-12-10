2df9c568a9ca365088e883e258d2f2657aef5ff2
jais260695 <aj936563@gmail.com>
2021-12-10 18:30:49 +0530

    Update Uncrossed_Lines.cs

--- Uncrossed_Lines.cs
+++ Uncrossed_Lines.cs
@@ -1,23 +1,20 @@
 public class Solution {
     public int MaxUncrossedLines(int[] nums1, int[] nums2) {
         int n1 = nums1.Count();
-        int n2 = nums2.Count();
-        
-        int[,] dp = new int[n1+1,n2+1];
-        
-        dp[0,0] = 0;
-        
-        for(int i=1;i<=n2;i++)
+        int n2 = nums2.Count();        
+        int[,] dp = new int[n1+1,n2+1];        
+        dp[0,0] = 0;        
+        for(int i=1;i<=n1;i++)
         {
-            for(int j=1;j<=n1;j++)
+            for(int j=1;j<=n2;j++)
             {
-                if(nums2[i-1]==nums1[j-1])
+                if(nums1[i-1]==nums2[j-1])
                 {
-                    dp[j,i] = 1 + dp[j-1,i-1];
+                    dp[i,j] = 1 + dp[i-1,j-1];
                 }
                 else
                 {
-                    dp[j,i] = Math.Max(dp[j,i-1],dp[j-1,i]);
+                    dp[i,j] = Math.Max(dp[i,j-1],dp[i-1,j]);
                 }
             }
         }
