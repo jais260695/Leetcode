23fb5cd1c80ea7e15cecb62acb0c7ed24c458e5e
jais260695 <aj936563@gmail.com>
2021-12-11 08:51:56 +0530

    Update Palindrome_Partitioning.cs

--- Palindrome_Partitioning.cs
+++ Palindrome_Partitioning.cs
@@ -1,34 +1,47 @@
 public class Solution {
-    List<List<string>> result = new List<List<string>>();
-    public bool isPalindrome(string s, int i, int j)
+    Dictionary<int,List<List<string>>> dp = new  Dictionary<int,List<List<string>>>();
+    public IList<IList<string>> Solve(string s, int i, int n,bool[,] isPalindrome)
     {
-        while(i<j && s[i]==s[j])
+        if(dp.ContainsKey(i)) return dp[i].ToList<IList<string>>();
+        List<List<string>> result = new List<List<string>>();
+        for(int j = i;j<n;j++)
         {
-            i++;
-            j--;
-        }
-        
-        return i>=j;
-    }
-    public void Solve(string s,int index, int n, List<string> ans)
-    {
-        if(index>=n) 
-        {
-            result.Add(new List<string>(ans));
-            return;
-        }
-        for(int i = index;i<n;i++)
-        {
-            if(isPalindrome(s,index,i))
+            if(isPalindrome[i,j])
             {
-                ans.Add(s.Substring(index,i-index+1));
-                Solve(s,i+1,n,ans);
-                ans.RemoveAt(ans.Count()-1);
+                if(j+1==n)
+                {
+                    result.Add(new List<string>(new List<string>(){s.Substring(i,j-i+1)}));
+                }
+                else
+                {
+                    foreach(List<string> t in Solve(s,j+1,n,isPalindrome))
+                    {
+                            List<string> temp = new List<string>(t);
+                            temp.Insert(0,s.Substring(i,j-i+1));
+                            result.Add(new List<string>(temp));
+                    }
+                }
             }
         }
+        dp.Add(i,result);
+        return result.ToList<IList<string>>();
     }
     public IList<IList<string>> Partition(string s) {
-         Solve(s,0,s.Length,new List<string>());
-        return result.ToList<IList<string>>();
+        int n = s.Length;
+        bool[,] isPalindrome = new bool[n,n];
+        for(int i=0;i<n-1;i++)
+        {
+            isPalindrome[i,i] = true;
+            isPalindrome[i,i+1] = s[i]==s[i+1];
+        }
+        isPalindrome[n-1,n-1] = true;
+        for(int k=2;k<n;k++)
+        {
+            for(int i=0,j=k;j<n;i++,j++)
+            {
+                isPalindrome[i,j] = (s[i]==s[j] && isPalindrome[i+1,j-1]);
+            }
+        }
+        return Solve(s,0,s.Length,isPalindrome);
     }
 }
