cf0657bd7d6922850230406aa883c58a72147e19
jais260695 <aj936563@gmail.com>
2021-12-14 18:15:42 +0530

    Create Minimum_Absolute_Difference_in_BST.cs
    Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
    
    Example 1:
    
    Input: root = [4,2,6,1,3]
    Output: 1
    Example 2:
    
    Input: root = [1,0,48,null,null,12,49]
    Output: 1
    
    Constraints:
    
    The number of nodes in the tree is in the range [2, 104].
    0 <= Node.val <= 105
    
    Note: This question is the same as 783: https://leetcode.com/problems/minimum-distance-between-bst-nodes/

---
+++ Minimum_Absolute_Difference_in_BST.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    List<int> temp = new List<int>();
+    public void inorder(TreeNode root)
+    {
+        if(root==null) return;
+        inorder(root.left);
+        temp.Add(root.val);
+        inorder(root.right);
+    }
+    public int GetMinimumDifference(TreeNode root) {
+        inorder(root);
+        int ans = int.MaxValue;
+        for(int i=1;i<temp.Count();i++)
+        {
+            if(temp[i]-temp[i-1]<ans) ans = temp[i]-temp[i-1];
+        }
+        return ans;
+    }
+}
