896f1373a29d8d3dc8ac5bb84be3f2220fcc2cd2
jais260695 <aj936563@gmail.com>
2021-12-15 19:40:51 +0530

    Update Burst_Balloons.cs

--- Burst_Balloons.cs
+++ Burst_Balloons.cs
@@ -16,33 +16,20 @@ public class Solution {
         {
             for(int i=0,j=k;j<n;i++,j++)
             {
-                int left = 1;
-                if(i-1>=0)
-                {
-                    left = nums[i-1];
-                }
-
-                int right = 1;
-                if(j+1<n)
-                {
-                    right = nums[j+1];
-                }
+                int left = i-1<0 ? 1 : nums[i-1];
+                int right = j+1>=n ? 1 : nums[j+1];
                 
-                dp[i,j] = left*nums[i]*right;
-                
-                if(i+1<n)
-                    dp[i,j]+=dp[i+1,j];
+                dp[i,j] = left*nums[i]*right + (i+1<n ? dp[i+1,j] : 0);
                 
                 for(int mid = i+1; mid<j;mid++)
                 {
-                    dp[i,j] = Math.Max(dp[i,mid-1]+(left*nums[mid]*right)+dp[mid+1,j],dp[i,j]);
+                    dp[i,j] = Math.Max(dp[i,mid-1] + (left*nums[mid]*right) + dp[mid+1,j], dp[i,j]);
                 }
                 
-                dp[i,j] = Math.Max( (j-1>=0?dp[i,j-1]:0)+(left*nums[j]*right),dp[i,j]);
+                dp[i,j] = Math.Max((j-1>=0 ? dp[i,j-1] : 0) + (left*nums[j]*right), dp[i,j]);
             }
         }
         
         return dp[0,n-1];
-        
     }
 }
