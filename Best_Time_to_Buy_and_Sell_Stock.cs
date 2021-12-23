6763f00426d33c0ce2a29b32a9d1f87090867b56
jais260695 <aj936563@gmail.com>
2021-12-23 20:26:27 +0530

    Update Best_Time_to_Buy_and_Sell_Stock.cs

--- Best_Time_to_Buy_and_Sell_Stock.cs
+++ Best_Time_to_Buy_and_Sell_Stock.cs
@@ -1,7 +1,6 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
         int n = prices.Count();
-        if(n==0)  return 0;
         int[] dp = new int[n];
         dp[0]  = prices[0];
         for(int i=1;i<n;i++)
@@ -12,9 +11,7 @@ public class Solution {
         int result = 0;
         for(int i = n-1;i>=0;i--)
         {
-            max = Math.Max(prices[i], max);
-            dp[i] = max - dp[i];
-            result = Math.Max(result,dp[i]);
+            result = Math.Max(result,prices[i]-dp[i]);
         }
         return result;
     }
