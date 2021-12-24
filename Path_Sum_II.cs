01deaabfe86c0de046bde5b064610378f808a85f
jais260695 <aj936563@gmail.com>
2021-12-24 18:42:55 +0530

    Update Path_Sum_II.cs

--- Path_Sum_II.cs
+++ Path_Sum_II.cs
@@ -14,34 +14,30 @@
 public class Solution {
     List<List<int>> res = new List<List<int>>();
     List<int> path = new List<int>();
-     public void PathSumUtil(TreeNode root, int sum) {
+     public void PathSumUtil(TreeNode root, int remainingSum) {
         if(root.left==null && root.right==null)
         {
             path.Add(root.val);
-            int c1 = path.Count()-1;
-            if(sum==root.val) 
+            if(root.val==remainingSum) 
             {
                 res.Add(new List<int>(path));
             }
-            path.RemoveAt(c1);
+            path.RemoveAt(path.Count()-1);
             return;
         }
         
         if(root.left!=null)
         {
             path.Add(root.val);
-            int c = path.Count()-1;
-            PathSumUtil(root.left,sum-root.val);
-            path.RemoveAt(c);
+            PathSumUtil(root.left,remainingSum-root.val);
+            path.RemoveAt(path.Count()-1);
         }
         if(root.right!=null)
         {
             path.Add(root.val);
-            int c = path.Count()-1;
-            PathSumUtil(root.right,sum-root.val);
-            path.RemoveAt(c);
+            PathSumUtil(root.right,remainingSum-root.val);
+            path.RemoveAt(path.Count()-1);
         }      
-        return;
     }
     public IList<IList<int>> PathSum(TreeNode root, int sum) {
         if(root==null)
