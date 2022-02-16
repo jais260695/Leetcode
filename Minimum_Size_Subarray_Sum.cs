175f7da6e2f9f654627c65baf455b7ed8d534292
jais260695 <aj936563@gmail.com>
2022-02-16 19:55:16 +0530

    Update Minimum_Size_Subarray_Sum.cs

--- Minimum_Size_Subarray_Sum.cs
+++ Minimum_Size_Subarray_Sum.cs
@@ -4,27 +4,22 @@ public class Solution {
         int i = 0;
         int j = 0;
         int result = n+1;
-        int sum = 0;
-        while(i<=j && j<n)
+        int sum = nums[0];
+        while(j<=i && i<n)
         {
-            while(j<n && sum<target)
+            if(sum<target)
             {
-                sum+=nums[j];
+                i++;
+                if(i==n) break;
+                sum+=nums[i];
+            }
+            else
+            {
+                result = Math.Min(result,i-j+1);
+                sum-=nums[j];
                 j++;
             }
-            j--;
-            if(sum>=target)
-                result = Math.Min(result,j-i+1);
-            sum-=nums[i];
-            i++;
-            j++;
         }
-        while(i<n)
-        {
-            if(sum>=target) result = Math.Min(result,j-i);   
-            sum-=nums[i];
-            i++;
-        }
-        return result==n+1 ? 0 : result;;
+        return result==n+1 ? 0 : result;
     }
 }
