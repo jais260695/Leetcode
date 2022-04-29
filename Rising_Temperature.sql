83d4a451f601c94b1b38394bc2d897d8182625ec
jais260695 <aj936563@gmail.com>
2022-04-29 07:59:20 +0530

    Update Rising_Temperature.sql

--- Rising_Temperature.sql
+++ Rising_Temperature.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 select W.id
-from weather W inner join Weather W1
-ON W1.recorddate = DATEADD(day,-1,W.recorddate)
+from weather W join Weather W1
+ON W.recorddate = DATEADD(day,1,W1.recorddate)
  and W.temperature > W1.temperature;
