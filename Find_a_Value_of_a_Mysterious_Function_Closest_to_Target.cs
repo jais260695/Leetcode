2bafc90a677ce8b18dbadbbc64b8fdbaeb4fe102
jais260695 <aj936563@gmail.com>
2022-05-15 10:34:04 +0530

    Create Find_a_Value_of_a_Mysterious_Function_Closest_to_Target.cs
    Winston was given the above mysterious function func. He has an integer array arr and an integer target and he wants to find the values l and r that make the value |func(arr, l, r) - target| minimum possible.
    
    Return the minimum possible value of |func(arr, l, r) - target|.
    
    Notice that func should be called with the values l and r where 0 <= l, r < arr.length.
    
    Example 1:
    
    Input: arr = [9,12,3,7,15], target = 5
    Output: 2
    Explanation: Calling func with all the pairs of [l,r] = [[0,0],[1,1],[2,2],[3,3],[4,4],[0,1],[1,2],[2,3],[3,4],[0,2],[1,3],[2,4],[0,3],[1,4],[0,4]], Winston got the following results [9,12,3,7,15,8,0,3,7,0,0,3,0,0,0]. The value closest to 5 is 7 and 3, thus the minimum difference is 2.
    Example 2:
    
    Input: arr = [1000000,1000000,1000000], target = 1
    Output: 999999
    Explanation: Winston called the func with all possible values of [l,r] and he always got 1000000, thus the min difference is 999999.
    Example 3:
    
    Input: arr = [1,2,4,8,16], target = 0
    Output: 0
    
    Constraints:
    
    1 <= arr.length <= 105
    1 <= arr[i] <= 106
    0 <= target <= 107

---
+++ Find_a_Value_of_a_Mysterious_Function_Closest_to_Target.cs
@@ -0,0 +1,86 @@
+public class Solution {
+    int[] segmentTree;
+    
+    public int ConstructSegmentTree(int[] arr, int l, int r, int i)
+    {
+        if(l==r)
+        {
+            return segmentTree[i] = arr[l];
+        }
+        
+        int mid = l + (r-l)/2;
+        
+        return segmentTree[i] = ConstructSegmentTree(arr, l, mid, 2*i+1) & ConstructSegmentTree(arr, mid+1, r, 2*i+2);
+    }
+    
+    public int GetRangeAND(int ss, int se, int l, int r, int i)
+    {
+        if(ss>=l && se<=r)
+        {
+            return segmentTree[i];
+        }
+        else if(ss>r || se<l)
+        {
+            return (1<<30) -1;
+        }
+        
+        int mid = ss + (se-ss)/2;
+        
+        return GetRangeAND(ss,mid,l,r,2*i+1) & GetRangeAND(mid+1,se,l,r,2*i+2);
+    }
+    
+    public int BinarySearch(int l , int r, int target)
+    {
+        int i = l;
+        int j = r;
+        while(l<=r)
+        {
+            int mid = l+ (r-l)/2;
+            int t = GetRangeAND(0,j,i,mid,0);
+            if(t>=target)
+            {
+                l = mid+1;
+            }
+            else
+            {
+                r = mid-1;
+            }
+        }
+        
+        return r;
+    }
+
+    
+    public int ClosestToTarget(int[] arr, int target) {
+        int n = arr.Count();
+        
+        //Calculate the segment tree length
+        int x = (int) (Math.Ceiling(Math.Log(n) / Math.Log(2)));
+        int N = 2*(int)Math.Pow(2,x) - 1;
+        
+        segmentTree = new int[N];
+        
+        //Construct segment tree of range AND to efficiently fetch any range AND operations 
+        ConstructSegmentTree(arr, 0, n-1, 0);
+        
+        int min = int.MaxValue;
+        
+        for(int i=0;i<n;i++)
+        {
+            //As AND operartion will casuse any range in decreasing order, so we can use binary search
+            //Proof: [2,6,8] 2=0010, 6 = 0110 , 8 = 1000 . Now 2&6 = 0010 & 0110 =  0010 = 2, and  2 & 8 = 0010 & 1000 = 0
+            
+            // Minimum of differnce of target to just greater and just smaller value will give the required result
+            int p = BinarySearch(i,n-1,target);
+            int justGreater = GetRangeAND(0,n-1,i,p,0);
+            min = Math.Min(min,Math.Abs(target-justGreater));
+            if(p<n-1)
+            {
+                int justSmaller = GetRangeAND(0,n-1,i,p+1,0);
+                min = Math.Min(min,Math.Abs(target-justSmaller));
+            }
+        }
+        
+        return min;
+    }
+}
