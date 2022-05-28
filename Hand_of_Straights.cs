98a21d084cafa9cacfb3b300d59df51403ce5fe5
jais260695 <aj936563@gmail.com>
2022-05-28 22:47:42 +0530

    Create Hand_of_Straights.cs
    Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
    
    Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
    
    Example 1:
    
    Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
    Output: true
    Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
    Example 2:
    
    Input: hand = [1,2,3,4,5], groupSize = 4
    Output: false
    Explanation: Alice's hand can not be rearranged into groups of 4.
    
    Constraints:
    
    1 <= hand.length <= 104
    0 <= hand[i] <= 109
    1 <= groupSize <= hand.length

---
+++ Hand_of_Straights.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public bool IsNStraightHand(int[] hand, int groupSize) {
+        int n = hand.Count();
+        if(n%groupSize!=0) return false;
+        Array.Sort(hand);
+        int result = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(hand[i]==-1) continue;
+            int count = 1;
+            int val = hand[i]+1;
+            hand[i] = -1;
+            int j = i+1;
+            while(j<n && count<groupSize)
+            {
+                if(hand[j]!=-1 && hand[j]==val)
+                {
+                    count++;
+                    val = hand[j]+1;
+                    hand[j] = -1;
+                }
+                
+                j++;
+            }
+            
+            if(count==groupSize) result++;
+
+        }
+        
+        return result == n/groupSize;
+    }
+}
