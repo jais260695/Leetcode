8e02dfd810e03f1f3a2029edc33f062c9bd0a601
jais260695 <aj936563@gmail.com>
2022-07-19 21:17:53 +0530

    Update Best_Time_To_Buy_And_Sell_Stock_IV.cs

--- Best_Time_To_Buy_And_Sell_Stock_IV.cs
+++ Best_Time_To_Buy_And_Sell_Stock_IV.cs
@@ -1,28 +1,52 @@
 public class Solution {
-    // DP + Memoization approach
-    public int[,,] dp;
     
-    public int FindResult(int[] prices, int i, int n, int flag, int limit)
+    int[,,] dp;   
+    int n;
+    int K;
+    public int FindResult(int[] prices, int i, int flag, int limit)
     {
-        if(i==n || limit==0) return 0;
+        //if all the days are covered or transaction limit is exhausted then return 0
+        if(i==n || limit==K) return 0;
         
-        if(dp[flag,limit,i]!=0) return dp[flag,limit,i];
-        int val =FindResult(prices,i+1,n,flag,limit);
+        //If current state is already solved then return result
+        if(dp[flag,limit,i]!=int.MinValue) return dp[flag,limit,i];
+        
+        //Chose to skip this day i.e.; don;t buy/sell and go to next day
+        int val = FindResult(prices,i+1,flag,limit);
+        
         if(flag==0)
         {            
-            val = Math.Max(val ,FindResult(prices,i+1,n,1,limit)-prices[i]);   
+            //take maximum after skipping cuurent day OR chose to buy on current day 
+            val = Math.Max(val, -prices[i] + FindResult(prices,i+1,1,limit));   
         }
         else
         {  
-            val =  Math.Max(val,prices[i] + FindResult(prices,i+1,n,0,limit-1));
+            //take maximum after skipping cuurent day OR chose to sell on current day and increase transaction limit
+            val =  Math.Max(val, prices[i] + FindResult(prices,i+1,0,limit+1));
         }
+        
         dp[flag,limit,i] = val;
         return val;
     }
     
-    public int MaxProfit(int k,int[] prices) {
-        int n = prices.Count();
-        dp = new int[2,k+1,n];
-        return FindResult(prices,0,n,0,k);
+    public int MaxProfit(int k, int[] prices) {
+        K=k;
+        n = prices.Count();
+        int transactionType = 2;// 0  when user can buy/skip and 1 when user can sell/skip
+        int transactionLimit = K+1;// upto K transactions allowed
+        dp = new int[transactionType,transactionLimit,n];
+        
+        //Initialize all result to minus infinity
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<K;j++)
+            {
+                for(int l=0;l<n;l++)
+                {
+                    dp[i,j,l] = int.MinValue;
+                }
+            }
+        }
+        return FindResult(prices,0,0,0);
     }
 }
