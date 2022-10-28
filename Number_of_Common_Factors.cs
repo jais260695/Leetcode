4a80d88e5100c9778d0fd56ddaf8fb34dfadc877
jais260695 <aj936563@gmail.com>
2022-10-28 08:09:06 +0530

    Create Number_of_Common_Factors.cs
    Given two positive integers a and b, return the number of common factors of a and b.
    
    An integer x is a common factor of a and b if x divides both a and b.
    
    Example 1:
    
    Input: a = 12, b = 6
    Output: 4
    Explanation: The common factors of 12 and 6 are 1, 2, 3, 6.
    Example 2:
    
    Input: a = 25, b = 30
    Output: 2
    Explanation: The common factors of 25 and 30 are 1, 5.
    
    Constraints:
    
    1 <= a, b <= 1000

---
+++ Number_of_Common_Factors.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int CommonFactors(int a, int b) {
+        int count = 0;
+        
+        for(int i=1;i<=Math.Min(a,b); i++)
+        {
+            if(a%i==0 && b%i==0) count++;
+        }
+        
+        return count;
+    }
+}
