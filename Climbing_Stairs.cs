0d29faa9f7ff688f47ac9ac567db354474a02ded
jais260695 <aj936563@gmail.com>
2022-12-12 08:52:36 +0530

    Update Climbing_Stairs.cs

--- Climbing_Stairs.cs
+++ Climbing_Stairs.cs
@@ -1,20 +1,9 @@
 public class Solution {
-    public int ClimbStairs(int n) {
-        
-        if(n<=2) return n;
+    public int ClimbStairs(int n) {        
         int[] dp = new int[n+1];
-        
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        
-        for(int i=3;i<=n;i++)
-        {
-            
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        
-        return dp[n];
-        
+        dp[0] = 1;
+        dp[1] = 1;        
+        for(int i=2;i<=n;i++) dp[i] = dp[i-1] + dp[i-2];    
+        return dp[n];        
     }
 }
